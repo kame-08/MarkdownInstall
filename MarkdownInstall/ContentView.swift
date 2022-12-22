3222ffb425bcd0a899ecb90ff732a46039cb4398
Kame2021 <21cm0108@jec.ac.jp>
2022-12-23 00:06:14 +0900

    Initial Commit

---
+++ MarkdownInstall/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarkdownInstall
+//
+//  Created by Ryo on 2022/12/23.
+//
+
+import SwiftUI
+import MarkdownViewSample
+
+struct ContentView: View {
+    var body: some View {
+        MarkdownView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarkdownInstall/MarkdownInstallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownInstallApp.swift
+//  MarkdownInstall
+//
+//  Created by Ryo on 2022/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownInstallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
